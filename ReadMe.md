e2faaef9e93d92e778d293accd684865da059495
HB L <39536628+IceTwilight@users.noreply.github.com>
2020-01-12 18:11:20 -0500

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,51 @@
+## Initial Setup
+* Add Landing Page
+* And Campgrounds Page that list all campgrounds
+
+Each campground has
+	name
+	image
+
+
+## Creating New campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+## Style the campground page
+* Add a better header/footer
+* make campground display in a grid
+
+## Style the navbar and Form
+* Add a navbar to all templates
+* Style the new campground form
+
+## Add mongoose
+* Install and configure mongoose
+* Setup campground model
+* Use campground model inside of routes.
+
+## Show Page
+* Review the RESTful routes we've seen so far
+* Add description to campground model
+* Show db.collection.drop()
+* Add a show route/template
+
+# Refactor Mongoose Code
+* Create a model directory
+* use module.exports
+* require everything correctly
+
+# Add seeds File
+* Add a seeds.js file
+* Run the seeds file every time the server starts
+
+# Add the comment model
+* make our error go away
+* display comments on campground show page
+
+# Comment new/create
+* discuss nested routes
+* add the comment new and create routes
+* add the new comment form
--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var campground_model = require("./models/campground");
+var comment_model = require("./models/comment");
+var user_model = require("./models/user");
+var seedDB = require("./seeds");
 
+seedDB();
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg"},
-    {name: "Titty", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQLOntxCNn67DIqFWkkpjucQG_sVu4VCAQLxtLjd1An37Fapiy&s"},
-    {name: "Bully", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ9ykdpP5hp3IwrjCL7BTVZUeFMrtLmvfPsK0i2TBRNf0_PCOi&s"},
-    {name: "Salmon", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg"},
-    {name: "Titty", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQLOntxCNn67DIqFWkkpjucQG_sVu4VCAQLxtLjd1An37Fapiy&s"},
-    {name: "Bully", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ9ykdpP5hp3IwrjCL7BTVZUeFMrtLmvfPsK0i2TBRNf0_PCOi&s"},
-    {name: "Salmon", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg"},
-    {name: "Titty", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQLOntxCNn67DIqFWkkpjucQG_sVu4VCAQLxtLjd1An37Fapiy&s"},
-    {name: "Bully", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ9ykdpP5hp3IwrjCL7BTVZUeFMrtLmvfPsK0i2TBRNf0_PCOi&s"}
-];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // get all campgrounds from DB
+    campground_model.find({}, function(err, all_campgrounds){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: all_campgrounds});
+        }
+    });
+    
 });
 
 app.get("/campgrounds/new", function(req, res){ // show the form send the data to the post route
@@ -34,9 +39,31 @@ app.post("/campgrounds", function(req, res){// new -> campgrounds+POST
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var img = req.body.image;
-    campgrounds.push({name:name, image:img});
-    //redirect back to campgrounds
-    res.redirect("/campgrounds");
+    var description = req.body.description;
+    var new_campground = {name:name, image:img, description:description};
+    campground_model.create(new_campground, function(err, newlyCreated){
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect back to campgrounds
+            res.redirect("/campgrounds");
+        }
+    })
+    // campgrounds.push({name:name, image:img});
+    
+});
+
+// Show more info about one campground - id
+app.get("/campgrounds/:id", function(req, res){
+    campground_model.findById(req.params.id).populate("comments").exec(function(err, found){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(found);
+            res.render("show", {campground: found});
+        }
+    });
+    
 });
 
 app.listen(3000, function(){
